880f59b8e1865150ffe70a02093e123f154bef7b
Adrian Dmitroca <dmitroca.adrian@gmail.com>
2015-12-03 17:59:16 +0100

    Root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  root to: 'visitors#index'
+
   resources :students do
     get :subjects
   end
 
-  root to: 'students#index'
   resources :teachers
   resources :report_subjects
   get '/reports/subjects', to: 'reports#subjects'

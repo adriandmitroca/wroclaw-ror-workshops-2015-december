374d6b042a71c644ffb11c8de4144bc3b1b8b637
Adrian Dmitroca <dmitroca.adrian@gmail.com>
2015-12-03 15:24:01 +0100

    Missing routes, controllers and variables
    Missing ReportSubject routes and Devise routes/traits covers approx. 50% of failing tests.

---
+++ app/controllers/report_subjects_controller.rb
@@ -0,0 +1,2 @@
+class ReportSubjectsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   resources :students do
     get :subjects
   end
+
+  root to: 'students#index'
+  resource :teachers
+  resource :report_subjects
+  get '/reports/subjects', to: 'reports#subjects'
 end
